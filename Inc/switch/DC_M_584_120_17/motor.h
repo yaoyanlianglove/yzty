0693e842a81b3d823976196f4696fbdd0292ce86
yaoyanliang <yaoyanliang.love@hotmail.com>
2020-10-19 16:29:33 +0800

    feat(switch): 双电机17档
    切换电机由94转改为120转。

--- Inc/switch/DC_M_584_120_17/motor.h
+++ Inc/switch/DC_M_584_120_17/motor.h
@@ -37,14 +37,14 @@ extern "C" {
 //切换电机参数
 #define     Q_R_STEP               8.0f
 #define     Q_R_SPEED_UP_TH        24
-#define     Q_R_SLOW_DOWN_TH       70
-#define     Q_R_OF_ONE_GEAR        94       //一档的转数
-#define     Q_R_OF_ON_GEAR         10       //在档位上的转数
-#define     Q_OVER_ON_GEAR         3        //比10大3说明超转数未找到挡位
-#define     Q_TIME_OF_ONE_CYCLE    1        //电机运行的周期延时1us  代码固有周期3.5us：电机控制while(1)的周期 
+#define     Q_R_SLOW_DOWN_TH       100
+#define     Q_R_OF_ONE_GEAR        120       //一档的转数
+#define     Q_R_OF_ON_GEAR         10        //在档位上的转数
+#define     Q_OVER_ON_GEAR         3         //比10大3说明超转数未找到挡位
+#define     Q_TIME_OF_ONE_CYCLE    1         //电机运行的周期延时1us  代码固有周期3.5us：电机控制while(1)的周期 
 
-#define     Q_MOTOR_TURN_TIMEOUT        2000
-#define     Q_MOTOR_TURN_OVER_SPEED     5.0 //
+#define     Q_MOTOR_TURN_TIMEOUT         2000
+#define     Q_MOTOR_TURN_OVER_SPEED      5.0 //
 
 
 #define     DELAY_GEAR_REMOTE_SIGNAL  1
